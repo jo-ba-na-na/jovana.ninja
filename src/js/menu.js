cedad036510c9eced059b81557dd1a4469d926d2
Jovana Skoric <me@jovana.ninja>
2017-04-28 16:05:37 +0200

    add js toggle menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,3 +2,14 @@
 Waves.attach('.menu_btn');
 Waves.attach('#ham_wrap');
 Waves.init();
+
+(function(){
+ 
+    var menu = document.getElementById('menu'),
+        menuTrigger = document.getElementById('ham_wrap');
+
+    menuTrigger.addEventListener('click', function(e) {
+        e.preventDefault();
+        menu.classList.toggle('menu_open');
+    });
+})()
