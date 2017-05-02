d67976293c67237521e43b1702fbaf1cddddeaea
Jovana Skoric <me@jovana.ninja>
2017-05-02 03:17:16 +0200

    close menu on outside click

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,16 +8,31 @@ Waves.init();
     var menu = document.getElementById('menu'),
         menuTrigger = document.getElementById('ham_wrap'),
         overlay = document.getElementById('overlay'),
-        x = document.getElementById('x_wrap');
+        x = document.getElementById('x_wrap'),
+        sideMenu = document.querySelector('#menu .side_menu');
+    
+    var isOpen = false;
 
     function menuToggle(e) {
         e.preventDefault();
+        e.stopPropagation();
         menu.classList.toggle('menu_open');
         overlay.classList.toggle('overlay_visible');
+        isOpen = !isOpen;
     }
 
     menuTrigger.addEventListener('click', menuToggle);
 
     x.addEventListener('click', menuToggle);
+
+    document.body.addEventListener('click', function(e) {
+        if (isOpen) {
+            menuToggle(e);
+        }
+    });
+
+    sideMenu.addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
 
 })()
